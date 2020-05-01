a41d21b8e6c52a18983f278fba80dc481d003de6
rigo0523 <limon1285@gmail.com>
2020-04-30 21:11:50 -0500

    updated

--- index.js
+++ index.js
@@ -86,13 +86,13 @@ Car.prototype.fill = function(gallons){
   return this.tank += gallons;
 };
 
-// Car.prototype.drive = function(distance){
-//   this.odometer += distance;
-//   (this.tank -= (distance / this.milesPerGallon));
-//   if(this.tank == 0){
-//     return `I ran out of fuel at ${this.odometer} miles!`;
-//   }
-// };
+Car.prototype.drive = function(distance){
+  this.odometer += distance;
+  (this.tank -= (distance / this.milesPerGallon));
+  if(this.tank === 0){
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
+};
 
 
 /*
